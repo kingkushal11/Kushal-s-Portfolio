43484c5810b78836fc7ecad53dc8ea54439ceee5
Kushal Khattri <kushalk62@gmail.com>
2022-06-08 23:20:05 -0700

    added my name to the header and added resume and about me button

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CTA = () => {
+    return (
+        <div className='cta'>
+            <a href="">Download Resume</a>
+            <a href="#contact">About Me</a>
+        </div>
+    )
+}
+
+export default CTA
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,11 @@ import "./header.css"
 const Header = () => {
     return (
         <header>
-            <div className="container header__container"></div>
+            <div className="container header__container">
+                <h5>Hello I'm</h5>
+                <h1>Kushal Khattri</h1>
+                <h5 className="text-light">Fullstack Developer</h5>
+            </div>
         </header>
     )
 }

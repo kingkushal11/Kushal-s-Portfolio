6e3fb9dc3f8e0eaabb01be149f583070bb6a48d1
Kushal Khattri <kushalk62@gmail.com>
2022-06-10 21:59:38 -0700

    added nav bar to the page and gave it a style and icons. made it fancy

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import "./nav.css"
+import { BiHome } from 'react-icons/bi'
+import { BiUserCircle } from 'react-icons/bi'
+import { GiSkills } from 'react-icons/gi'
+import { RiContactsBookFill } from 'react-icons/ri'
 
 const Nav = () => {
     return (
-        <div>Nav</div>
+        <nav>
+            <a href="#" className='active'><BiHome /></a>
+            <a href="#about"><BiUserCircle /></a>
+            <a href="#skills"><GiSkills /></a>
+            <a href="#contact"><RiContactsBookFill /></a>
+        </nav>
     )
 }
 
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./skills.css"
+
+const skills = () => {
+    return (
+        <section id='skills'>Skills</section>
+    )
+}
+
+export default skills
